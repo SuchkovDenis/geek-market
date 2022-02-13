b37664877a637c9d2ffd131cf36d5a461d52e1dc
suchkovdenis <ny1828@yandex.ru>
2022-02-13 21:19:36 +0300

    styles refactor

--- src/main/java/ru/gb/springbootdemoapp/controller/ProductController.java
+++ src/main/java/ru/gb/springbootdemoapp/controller/ProductController.java
@@ -26,7 +26,7 @@ public class ProductController {
     List<ProductDto> students =  productService.getAll().stream()
         .map(productMapper::productToProductDto).collect(Collectors.toList());
     model.addAttribute("products", students);
-    return "product_list";
+    return "home";
   }
 
   @GetMapping("/info/{id}")
